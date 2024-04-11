21d5fcaedc7faac0d9dd7a9a675e98da2129458e
walaeddine01 <walaaeddine33@gmail.com>
2024-04-12 00:48:54 +0200

    I made the changes on 2024-04-12 00:48:54

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
 """
-
+This module contains the pascal method
 """
+
+
 def pascal_triangle(n):
     """
+    Pascal method
     """
     _list1 = []
     if n <= 0:
         return _list1
     for i in range(n):
-        _list1.append([1])
-        j = 0
-        for j in range(i):
-            if i == j + 1 and i != 0:
-                _list1[i].append(1)
-            elif i == j:
-                 _list1[i].append(1)
-            else:
-                print(i, j)
-                _list1[i].append(_list1[i - 1][j - 1] + _list1[i - 1][j])
-                for row in _list1:
-                    print("[{}]".format(",".join([str(x) for x in row])))
+        row = [1]
+        for j in range(1, i):
+            row.append(_list1[i - 1][j - 1] + _list1[i - 1][j])
+        if i > 0:
+            row.append(1)
+        _list1.append(row)
 
     return _list1
