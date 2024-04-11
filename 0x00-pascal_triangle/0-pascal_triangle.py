055729594e7e61caa4e725ca574657c5eb396fb8
walaeddine01 <walaaeddine33@gmail.com>
2024-04-11 22:59:58 +0200

    I made the changes on 2024-04-11 22:59:58

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
+"""
+
+"""
 def pascal_triangle(n):
+    """
+    """
     _list1 = []
     if n <= 0:
         return _list1
     for i in range(n):
         _list1.append([1])
-        for j in range(n):
+        j = 0
+        for j in range(i):
             if i == j + 1 and i != 0:
                 _list1[i].append(1)
-                break
-            elif i == 0:
-                break
+            elif i == j:
+                 _list1[i].append(1)
             else:
+                print(i, j)
                 _list1[i].append(_list1[i - 1][j - 1] + _list1[i - 1][j])
+                for row in _list1:
+                    print("[{}]".format(",".join([str(x) for x in row])))
+
     return _list1
