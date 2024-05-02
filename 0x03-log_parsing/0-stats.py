7212c191a156967ddf7d45da6acaf45fcb26ecfd
walaeddine01 <walaaeddine33@gmail.com>
2024-05-02 16:56:19 +0200

    I made the changes on 2024-05-02 16:56:19

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -24,12 +24,14 @@ try:
             response_size = int(match.group(2))
             sum += response_size
         if len(data) % 10 == 0:
-            print("File size: " + str(sum))
+            print("File size:", str(sum))
             data.sort()
             for code in set(data):
                 print("{}: {}".format(code, data.count(code)))
 except KeyboardInterrupt:
-    print("File size: " + str(sum))
+    pass
+finally:
+    print("File size:", str(sum))
     data.sort()
     for code in set(data):
         print("{}: {}".format(code, data.count(code)))
