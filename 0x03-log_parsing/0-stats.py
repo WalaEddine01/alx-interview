f143da81d872e8e081c486590b6d5f94f39e87a6
walaeddine01 <walaaeddine33@gmail.com>
2024-05-02 17:06:35 +0200

    I made the changes on 2024-05-02 17:06:35

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,37 @@
 #!/usr/bin/python3
-"""
-This script reads from standard input and computes metrics
-"""
-import re
+'''A script that reads stdin line by line and computes metrics'''
+
+
 import sys
 
-
-pat = (
-    r'[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+ - \[[0-9]+-[0-9]+-[0-9]+ '
-    r'[0-9]+:[0-9]+:[0-9]+\.[0-9]+\] "GET /projects/260 HTTP/1\.1" '
-    r'([0-9]+) ([0-9]+)'
-)
-
-data = []
-sum = 0
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
 try:
     for line in sys.stdin:
-        match = re.match(pat, line)
-        if match:
-            data.append(match.group(1))
-            response_size = int(match.group(2))
-            sum += response_size
-        if len(data) % 10 == 0:
-            print("File size:", str(sum))
-            data.sort()
-            for code in set(data):
-                print("{}: {}".format(code, data.count(code)))
-except KeyboardInterrupt:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
+
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
     pass
 
 finally:
-    print("File size:", str(sum))
-    data.sort()
-    for code in set(data):
-        print("{}: {}".format(code, data.count(code)))
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
