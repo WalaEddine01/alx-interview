12d01387190a76ea778e6b8bc889c942a591eefd
walaeddine01 <walaaeddine33@gmail.com>
2024-05-24 02:06:14 +0200

    I made the changes on 2024-05-24 02:06:14

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,9 +2,10 @@
 const argv = require('process').argv;
 const request = require('request');
 
-const endPointFilms = 'films'; const id = argv[2]; const endPointPeople = 'people'; const i = 1;
+const endPointFilms = 'films';
+const id = argv[2];
+const endPointPeople = 'people';
 const url = `https://swapi-api.alx-tools.com/api/${endPointFilms}/${id}/`;
-const urlPeople = `https://swapi-api.alx-tools.com/api/${endPointPeople}/${i}/`;
 const regex = /\/(\d+)\/$/;
 
 request(url, (error, response, body) => {
@@ -14,20 +15,26 @@ request(url, (error, response, body) => {
   }
 
   const characters = JSON.parse(body).characters;
-  const numbers = [];
-  for (const c of characters) {
-    const match = c.match(regex);
-    const lastNumber = match[1];
-    numbers.push(lastNumber);
-  }
+  const numbers = characters.map(c => c.match(regex)[1]);
   numbers.sort((a, b) => a - b);
-  for (const j of numbers) {
-    request(urlPeople.replace(i, j), (error, response, body) => {
+
+  let completedRequests = 0;
+  const names = new Array(numbers.length);
+
+  numbers.forEach((number, index) => {
+    const urlPeople = `https://swapi-api.alx-tools.com/api/${endPointPeople}/${number}/`;
+
+    request(urlPeople, (error, response, body) => {
       if (error) {
         console.error('Error with GET request:', error);
         return;
       }
-      console.log(JSON.parse(body).name);
+      names[index] = JSON.parse(body).name;
+      completedRequests++;
+
+      if (completedRequests === numbers.length) {
+        names.forEach(name => console.log(name));
+      }
     });
-  }
+  });
 });
