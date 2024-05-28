4feabc5355756e091d2ae2fde9e8f854c11e9af2
walaeddine01 <walaaeddine33@gmail.com>
2024-05-28 07:11:31 +0200

    I made the changes on 2024-05-28 07:11:31

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
 """
+this module contains the rotate_2d_matrix method
 """
+
+
+def rotate_2d_matrix(matrix):
+    '''
+    the rotate_2d_matrix method
+    '''
+    tempMat = []
+    n = len(matrix)
+    for i in range(n):
+        tempMat.append([0] * n)
+    for i in range(n):
+        for j in range(n):
+            tempMat[j][n - 1 - i] = matrix[i][j]
+    for i in range(n):
+        for j in range(n):
+            matrix[i][j] = tempMat[i][j]
