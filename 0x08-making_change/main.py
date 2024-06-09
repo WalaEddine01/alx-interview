ffe0de27cba48bc81847df67c5ce12a96dd773bb
walaeddine01 <walaaeddine33@gmail.com>
2024-06-09 20:12:17 +0200

    I made the changes on 2024-06-09 20:12:17

--- 0x08-making_change/main.py
+++ 0x08-making_change/main.py
@@ -5,6 +5,14 @@ Main file for testing
 
 makeChange = __import__('0-making_change').makeChange
 
-print(makeChange([1, 2, 25], 37))
-
-print(makeChange([1256, 54, 48, 16, 102], 1453))
+print(makeChange([1, 2, 5], 11))  # Output: 3 (11 = 5 + 5 + 1)
+print(makeChange([2], 3))  # Output: -1 (cannot form 3 with only coins of 2)
+print(makeChange([1, 4, 5], 8))  # Output: 2 (8 = 4 + 4)
+print(makeChange([1, 2, 5], 0))  # Output: 0 (total is 0, so 0 coins are needed)
+print(makeChange([5], 3))  # Output: -1 (cannot form 3 with only coins of 5)
+print(makeChange([1, 3, 4], 6))  # Output: 2 (6 = 3 + 3)
+print(makeChange([1, 2, 5], 100))  # Output: 20 (100 = 5*20)
+print(makeChange([7, 14], 49))  # Output: 7 (49 = 7*7)
+print(makeChange([1, 5, 10, 25], 30))  # Output: 2 (30 = 25 + 5)
+print(makeChange([3, 7, 8], 15))  # Output: 2 (15 = 7 + 8)
+print(makeChange([1, 5, 10, 21], 63)) 
