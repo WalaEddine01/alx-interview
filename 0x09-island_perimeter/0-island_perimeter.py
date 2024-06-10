4783aa5f2562eba16aa349c39d9351616d185a8c
walaeddine01 <walaaeddine33@gmail.com>
2024-06-11 00:08:18 +0200

    I made the changes on 2024-06-11 00:08:18

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This module for island problem
+"""
+
+
+def island_perimeter(grid):
+    """
+    island perimeter method
+    """
+    v = len(grid)
+    h = len(grid[0])
+    for i in range(v):
+        for j in range(h):
+            if grid[i][j] == 1:
+                if grid[i - 1][j] != 1:
+                    grid[i - 1][j] = grid[i - 1][j] + 2
+                if grid[i][j - 1] != 1:
+                    grid[i][j - 1] = grid[i][j - 1] + 2
+                if grid[i][j + 1] != 1:
+                    grid[i][j + 1] = 2 + grid[i][j + 1]
+                if grid[i + 1][j] != 1:
+                    grid[i + 1][j] = 2 + grid[i + 1][j]
+    s = 0
+    for i in grid:
+        for j in i:
+            if j != 1:
+                s = s + j
+
+    return int(s / 2)
---
+++ 0x09-island_perimeter/README.md
@@ -0,0 +1,6 @@
+<br clear="both">
+
+<h1 align="center">0x09-island_perimeter</h1>
+
+###
+
