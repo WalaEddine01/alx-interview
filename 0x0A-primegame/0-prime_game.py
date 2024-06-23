9aa62a28113033bc0f65cd1be54f3967446d2414
walaeddine01 <walaaeddine33@gmail.com>
2024-06-24 00:02:06 +0100

    I made the changes on 2024-06-24 00:02:06

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -9,14 +9,14 @@ def isWinner(x, nums):
     """
     Determine the winner of the game
     """
-    if x <= 0 or not nums or x > 10000:
+    if x <= 0 or not nums or x - 1 >= 10000:
         return None
 
     maria_wins = 0
     ben_wins = 0
 
     for num in nums:
-        if num > 10000:
+        if num >= 10000:
             return None
         numbers = list(range(2, num + 1))
         turn = 0  # maria = 0, ben = 1
